6e49cd0a9426d658964041ec9bbf7f0a86cb897d
Pedro Delgadillo <PJDelgadillo@student.fullsail.edu>
2023-11-20 18:35:19 -0600

    made an http request that returns a todo list added nodemon

--- server.js
+++ server.js
@@ -1,13 +1,27 @@
 const http = require("http");
 
-const server = http.createServer((req,res)=>{
-    const {headers, url, method} = req;
-    console.log(headers, url, method);
-    res.end();
+const todos = [
+  {
+    id: 1,
+    task: "task one",
+  },
+  {
+    id: 2,
+    task: "task two",
+  },
+  {
+    id: 3,
+    task: "task three",
+  },
+];
+
+const server = http.createServer((req, res) => {
+  res.setHeader("content-type", "application/json");
+  res.end(JSON.stringify({ success: true, method: req.method, data: todos }));
 });
 
 const PORT = 3000;
 
-server.listen(PORT, ()=>{
-console.log(`listening on port ${PORT}`);
+server.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
 });
