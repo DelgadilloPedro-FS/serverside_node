95035d5b3d65b381e97e7043e2a3e3c25b464afd
Pedro Delgadillo <PJDelgadillo@student.fullsail.edu>
2023-11-27 18:48:09 -0600

    added error handling for routes

--- app/index.js
+++ app/index.js
@@ -18,68 +18,90 @@ router.get("/", (req, res) => {
 
 //get by id
 router.get("/:id", (req, res) => {
-    const {id} = req.params;
-
-    const cat = DATA[id]
+  const { id } = req.params;
+  if (DATA[id]) {
+    const cat = DATA[id];
     res.status(200).json({
-        message: "GET by ID for cat/id",
-        metadata: {
-          hostname: req.hostname,
-          cat,
-          method: req.method,
-        },
-      });
-})
-
-
-router.post("/", (req, res) => {
-  const {data} = req.body;
-  DATA.push(data)
-  console.log(req.body)
-  res.status(200).json({
-    message: "POST to /cat",
-    metadata: {
-      hostname: req.hostname,
-      DATA,
-      method: req.method,
-    },
-  })
+      message: "GET by ID for cat/id",
+      metadata: {
+        hostname: req.hostname,
+        cat,
+        method: req.method,
+      },
+    });
+  } else {
+    res.status(404).send({ error: "Cat not found" });
+  }
 });
+
+//post method
+router.post("/", (req, res) => {
+  const { data } = req.body;
+  if (data) {
+    DATA.push(data);
+    res.status(200).json({
+      message: "POST to /cat",
+      metadata: {
+        hostname: req.hostname,
+        DATA,
+        method: req.method,
+      },
+    });
+  } else {
+    res.status(400).send({ error: "Missing data in request body" });
+  }
+});
+
 //update by id
-router.put("/:id",(req, res)=>{
-    const {id} = req.params;
-    const {data} = req.body;
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { data } = req.body;
+  if (DATA[id]) {
     DATA[id].catName = data.catName;
     res.status(200).json({
-        message: "update id /api/:id",
-        metadata: {
-          hostname: req.hostname,
-          DATA,
-          method: req.method,
-        },
-      })
-})
-router.delete("/:id",(req,res)=>{
-    const {id} = req.params;
-    const {data} = req.body;
-    const deletedData = DATA.splice(id,1)
-    res.status(200).json({
+      message: "update id /api/:id",
+      metadata: {
+        hostname: req.hostname,
+        DATA,
+        method: req.method,
+      },
+    });
+  } else {
+    res.status(400).send({ error: "Cat not found" });
+  }
+});
+
+//delete method
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const deletedData = DATA.splice(id, 1);
+  try {
+    if (DATA[id]) {
+      res.status(200).json({
         message: "Delete by id /cat",
         metadata: {
           hostname: req.hostname,
           deletedData,
           method: req.method,
         },
-      })
-})
+      });
+    } else {
+      res.status(400).send({ error: "Cat not found" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error: "Internal Server Error" });
+  }
+});
+
 app.use("/cat", router);
 
 module.exports = app;
 
 const DATA = [
-    {id:0,catName:"whiskers"},
-    {id:1,catName:"ham"},
-    {id:2,catName:"mittens"},
-    {id:3,catName:"bacon"},
-    {id:4,catName:"kitty"},
-]
+  { id: 0, catName: "whiskers" },
+  { id: 1, catName: "ham" },
+  { id: 2, catName: "mittens" },
+  { id: 3, catName: "bacon" },
+  { id: 4, catName: "kitty" },
+];
